14a172ff569bac6457810ffc6fc3e8a7f90bc7bd
adrian zambrano <adrianzamstrong@gmail.com>
2022-04-05 21:31:01 -0400

    discount-2

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,14 +12,19 @@ switch (question){
     break;
 }
 
-let getDiscount = alert("obten un 15% de descuento en tu primera compra suscribiendote!");
-let purchase = parseInt(prompt("ingresa el valor de tu compra"));
+let getDiscount = alert("obten un 15% de descuento en tu primera compra sobre $25.000 solo suscribiendote!");
+let purchase = parseFloat(prompt("ingresa el valor de tu compra"));
 let discount = purchase *(15/100) ;
 
 
 function finalPriceWithDiscount(){
     let totalPrice = purchase - discount;
+    if (purchase > 25000){
+        totalPrice= purchase;
+        console.log("no se aplica descuento");
+    }
     alert("El total de tu compra es " + totalPrice + ".");
+
 }
 
 let display = finalPriceWithDiscount();
