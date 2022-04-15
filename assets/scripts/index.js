1c1cc3123315cd30db3b0fd8373889f83a28d3a3
adrian zambrano <adrianzamstrong@gmail.com>
2022-04-14 23:57:53 -0400

    complete array

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -30,8 +30,9 @@ function finalPriceWithDiscount(){
 
 let display = finalPriceWithDiscount();
 }
+// -------------------------------------------------------------------------------//
 
-const packPremiun = {  //Objeto      <-------------------
+const packPremiun = {  //Objeto   1   <-------------------
     clases : 10,
     descuentos: {
         servicioTecnico: 10,
@@ -41,28 +42,29 @@ const packPremiun = {  //Objeto      <-------------------
     },
     cupoTours: "el durazno"
 }
+
 class Users {
-    constructor(nombre, email, edad, genero) {
+    constructor(nombre, email, edad) { // construcctor   1   <-------------------//
         this.nombre = nombre;
         this.email = email;
         this.edad = edad;
-        this.genero = genero;
         this.welcome = 'Gracias por registrarte en nuestro website, bienvenid@ ' + this.nombre;
     }
 } 
-const user1 = new Users("adrian","adrianironside@gmail.com", 21 ,true);
-const user2 = new Users("gabriela","magaa2508@gmail.com", 25, false);
-const user3 = new Users("carlos", "cezduk@yahoo.com", 48, true);
-const user4 = new Users("doja", "catdoja@hotmail.com", 31, false);
+const user1 = new Users("adrian","adrianironside@gmail.com", 21);
+const user2 = new Users("gabriela","magaa2508@gmail.com", 25);
+const user3 = new Users("carlos", "cezduk@yahoo.com", 48);
+const user4 = new Users("doja", "catdoja@hotmail.com", 31);
  
+let nombre = document.querySelector(".name");
+let mail = document.querySelector(".mail-input");
+let age = document.querySelector(".age");
+console.log(nombre.innerText);
+console.log(mail.innerText);
+console.log(age.innerText);
 const register = () =>{
-    let nombre = prompt("ingresa tu nombre");
-    let mail = prompt("ingresa tu email");
-    let age = parseInt(prompt("ingresa tu edad"));
-    let gender = confirm("si eres hombre click aceptar y si eres mujer click cancelar");
-
-    const user5 = new Users(nombre,mail,age,gender);
-    console.log(user5);
+    const user5 = new Users(nombre,mail,age);
+    return console.log(user5);
 }
 /*
 ========================================= Arrays ===============================================
@@ -78,12 +80,16 @@ class AddToCart {
     }
 }
 
-let productName = prompt("ingresa tu producto");
-let productPrice = parseInt(prompt("ingresa el precio"));
-let productqty = parseInt(prompt("ingresa la cantidad que deseas agregar"));
 
-let product1 = new AddToCart("guantes mtb", 10.999, 1);
+let product1 = new AddToCart("guantes mtb", 10.999, 1); 
+carrito.push(product1);                                          // <--- ejemplo
 
-const agregar = new AddToCart(productName, productPrice, productqty);
-const envion = carrito.push(agregar, product1);
-console.log(carrito)
+const trigger = () =>{
+    let productName = prompt("ingresa tu producto");
+    let productPrice = parseInt(prompt("ingresa el precio"));
+    let productQty = parseInt(prompt("ingresa la cantidad que deseas agregar"));
+    const agregar = new AddToCart(productName, productPrice, productQty);
+    const envion = carrito.push(agregar);
+    console.log(agregar)
+}
+ 
